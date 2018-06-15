9a03ac31caf7c91ec469bdaee8d2fc8261d22449
Arash <arash.ashrafnejad@gmail.com>
2018-06-15 18:56:14 +0300

    get daily twitter sentiments

---
+++ getSentiment.py
@@ -0,0 +1,160 @@
+import os
+import time
+import numpy as np
+import pandas as pd
+from textblob import TextBlob
+
+from df2gspread import df2gspread as d2g
+spreadsheet = '/spreadsheets/altcoin_predictions'
+
+
+coins = [
+'BTC',
+'TRX'
+,'XVG',
+'MCO','ETH','XRP','XLM','ADA', 'NEO',
+'EOS','ICX','BNB','ONT','NANO','IOTA','LTC','VEN','XMR',
+'ETC','OMG','WAN','QTUM','ELF','AION',
+'LINK','SNT','WAVES','BTG','STRAT','NULS',
+'SALT','STEEM','DGD','ZIL','WTC','DASH',
+'LSK','ENG','ZRX','XEM','ZEC','XZC','PPT','ARK',
+'BTS','GAS','GTO','HSR','FUN',
+'LRC','BLZ','KMD','KNC','AE','BCD','SYS',
+'VIA','RLC',
+'STORJ','PIVX','BNT','MANA'
+]
+
+
+coin_dict = {
+    'BTC': 'Bitcoin',
+    'TRX': 'TRON',
+    'XVG': 'Verge',
+    'MCO': 'Monaco',
+    'ETH': 'Ethereum',
+    'XRP': 'Ripple',
+    'XLM': 'Stellar',
+    'ADA': 'Cordano',
+    'NEO': 'NEO',
+    'EOS': 'EOS',
+    'ICX': 'ICON',
+    'BNB': 'Binance Coin',
+    'ONT': 'Ontology',
+    'NANO': 'Nano',
+    'IOTA': 'IOT',
+    'LTC': 'Litecoin',
+    'VEN': 'VeChain',
+    'XMR': 'Monero',
+    'ETC': 'Ethereum Classic',
+    'OMG': 'OmiseGO',
+    'WAN': 'Wanchain',
+    'QTUM': 'Qtum',
+    'ELF': 'aelf',
+    'AION': 'Aion',
+    'LINK': 'ChainLink',
+    'SNT': 'Status',
+    'WAVES': 'Waves',
+    'BTG': 'Bitcoin Gold',
+    'STRAT': 'Stratis',
+    'NULS': 'Nuls',
+    'SALT': 'SALT',
+    'STEEM': 'Steem',
+    'DGD': 'DigixDAO',
+    'ZIL': 'Zilliqa',
+    'WTC': 'Waltonchain',
+    'DASH': 'Dash',
+    'LSK': 'Lisk',
+    'ENG': 'Enigma',
+    'ZRX': '0x',
+    'XEM': 'NEM',
+    'ZEC': 'Zcash',
+    'XZC': 'ZCoin',
+    'PPT': 'Populous',
+    'ARK': 'Ark',
+    'BTS': 'BitShares',
+    'GAS': 'Gas',
+    'GTO': 'Gifto',
+    'HSR': 'Hshare',
+    'FUN': 'FunFair',
+    'LRC': 'Loopring',
+    'BLZ': 'Bluzelle',
+    'KMD': 'Komodo',
+    'KNC': 'Kyber Network',
+    'AE': 'Aeternity',
+    'BCD': 'Bitcoin Diamond',
+    'SYS': 'Syscoin',
+    'VIA': 'Viacoin',
+    'RLC': 'iExec',
+    'STORJ': 'Storj',
+    'PIVX': 'PIVX',
+    'BNT': 'Bancor',
+    'MANA': 'Decentraland'
+    }
+
+
+
+
+
+from datetime import datetime
+
+
+
+while(True):
+    nowDate = datetime.now()
+    UTC_OFFSET_TIMEDELTA = datetime.utcnow() - datetime.now()
+
+
+    start_date = datetime(nowDate.year, nowDate.month, nowDate.day, 0, 0)
+
+    sentiments = pd.DataFrame()
+
+    for coin in coins:
+        print(coin)
+        searchQuery = coin_dict[coin]+' '+coin
+        # get Twitter sentiments
+        try:
+            twData = pd.read_csv('/home/arash/BitPredict/data/%s_today_tweets.csv'%coin)
+        except:
+            os.system('python ./GetOldTweets/Exporter.py --querysearch "%s" --since %s --output ./data/%s_today_tweets.csv'%(searchQuery,start_date.strftime("%Y-%m-%d"),coin))
+            twData = pd.read_csv('/home/arash/BitPredict/data/%s_today_tweets.csv'%coin)
+            os.system('rm /home/arash/BitPredict/data/%s_today_tweets.csv'%coin)
+
+        twData['sentiment'] = np.zeros(len(twData))
+        for index, row in twData.iterrows():
+            # sentiment analysis
+            try:
+            	analysis = TextBlob(row['text'])
+            	twData.ix[index, 'sentiment'] = analysis.sentiment.polarity
+
+            except:
+            	twData.ix[index, 'sentiment'] = 0.0
+            	print('Got invalid row!')
+
+        	# convert to utc time
+        	local_datetime = datetime.strptime(row['date'], "%Y-%m-%d %H:%M")
+        	result_utc_datetime = local_datetime + UTC_OFFSET_TIMEDELTA
+        	result_utc_str = result_utc_datetime.strftime("%Y-%m-%d %H:%M")
+
+        	# round down to the nearest hour
+        	twData.ix[index, 'date'] = result_utc_str[:-2]+'00:00'
+
+            # group and average over hourly dates
+            sub_twData = twData.groupby('date').mean()[['sentiment']]
+
+            if coin == 'BTC':
+                sentiments = sub_twData
+                sentiments = sentiments.rename(columns={'sentiment': 'BTC'})
+            else:
+                sentiments[coin] = sub_twData['sentiment']
+    sentiments = sentiments.fillna(0)
+
+    # get average of sentimets over hours of today
+    avgSentiments = pd.DataFrame(sentiments.mean(axis=0).sort_values(ascending=False))
+    d2g.upload(avgSentiments, spreadsheet, 'avgSentiments')
+    print(avgSentiments.head())
+
+    # get hourly prices of today sorted based on last hour
+    sentiments = sentiments.transpose()
+    sentiments = pd.DataFrame(sentiments.sort_values(sentiments.columns[-1], ascending=False))
+    d2g.upload(sentiments, spreadsheet, 'sentiments')
+    sentiments.to_csv('/home/arash/BitPredict/data/today_sentiments.csv')
+    print(sentiments.head())
---
+++ getSpreads.py
@@ -0,0 +1,35 @@
+import binance
+
+
+
+binance_coins = [
+'TRX','XVG','NCASH'
+,'MCO','ETH','XRP','XLM','ADA','GRS','NEO'
+,'EOS','ICX','BNB','BCC','STORM','BAT','ONT','NANO','IOTA','LTC','VEN','XMR'
+,'ETC','IOST','OMG','SUB','WAN','NEBL','QTUM','MTL','ELF','GVT','AION'
+,'CLOAK'
+,'QLC','LINK','SNT','WAVES','BTG','ENJ','BQX','EDO','STRAT','POA','NULS','TRIG'
+,'SALT','STEEM','LEND','VIBE','BCPT','POWR','DGD','ZIL','CMT','WTC','DASH','POE'
+,'LSK','LUN','ENG','ZRX','XEM','ADX','WPR','ARN','ZEC','XZC','PPT','ARK','INS'
+,'CND','RCN','AMB','DLT','VIB','OST','BTS','GAS','BRD','DNT','GTO','HSR','FUN'
+,'CHAT','NAV','LRC','TNB','QSP','REQ','BLZ','KMD','APPC','KNC','AE','BCD','SYS'
+,'RPX','SNGLS','MDA','WABI','FUEL','TNT','VIA','MTH','GXS','EVX','RLC','CDT'
+,'AST','WINGS','YOYO','STORJ','PIVX','SNM','BNT','ICN','RDN','OAX','MANA','MOD'
+]
+
+spreads = {}
+
+while(True):
+    ticks = binance.tickers()
+    for coin in binance_coins:
+        tick = ticks[coin+'BTC']
+        bid = float(tick['bid'])
+        ask = float(tick['ask'])
+
+        spread = (ask/bid - 1)*100
+        spreads[coin] = spread
+
+    maxCoin = max(spreads, key=spreads.get)
+    maxSpread = spreads[maxCoin]
+
+    print '%s : %.2f'%(maxCoin, maxSpread)
